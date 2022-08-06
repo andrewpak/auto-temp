e77420536b15b2922ae4ccabfbf98dfa108e6492
andrewpak <andrew7pak@gmail.com>
2022-08-06 04:03:37 -0400

    added activate, still have to update Makefile.am

--- hello.c
+++ hello.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
+#include<gtk/gtk.h>
 
-int main()
+static void
+activate (GtkApplication* app,
+					gpointer				user_data)
 {
-	printf("hello world\n");
-	return 0;
+	GtkWidget *window;
+	window = gtk_application_window_new(app);
+	gtk_window_set_title (GTK_WINDOW (window), "Window");
+	gtk_window_set_default_size (GTK_WINDOW (window), 200, 300);
+	gtk_widget_show(window);
+}
+
+int main(int argc, char *argv[])
+
+{
+	GtkApplication * app;
+	int status;
+
+	app = gtk_application_new ("org.gtk.ex", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect (app, "activate", G_CALLBACK (activate),  NULL);
+	status = g_application_run (G_APPLICATION (app), argc, argv);
+	g_object_unref (app);
+
+	return status;
 }
