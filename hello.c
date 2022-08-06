ba6d84a4a12325cc901832915925a4d4d6133f3e
andrewpak <andrew7pak@gmail.com>
2022-08-06 04:50:01 -0400

    changed size of window

--- hello.c
+++ hello.c
@@ -8,7 +8,7 @@ activate (GtkApplication* app,
 	GtkWidget *window;
 	window = gtk_application_window_new(app);
 	gtk_window_set_title (GTK_WINDOW (window), "Window");
-	gtk_window_set_default_size (GTK_WINDOW (window), 200, 300);
+	gtk_window_set_default_size (GTK_WINDOW (window), 800, 800);
 	gtk_widget_show(window);
 }
 
