b3f0d10046801ed6af3271c5da231a8eb1a0c791
andrewpak <andrew7pak@gmail.com>
2022-08-11 03:27:46 -0400

    add child to window

--- main.c
+++ main.c
@@ -7,16 +7,20 @@ static void
 activate (GtkApplication* app, gpointer user_data)
 {
 	GtkWidget *window;
-	window = gtk_application_window_new(app);
 	GtkWidget *button;
 	GtkWidget *view;
-	view = gtk_text_view_new();
 	GtkTextBuffer *buffer;
-	buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (view));
-	gtk_text_buffer_set_text(buffer, "This is some text", -1);
+
+	window = gtk_application_window_new(app);
 	button = gtk_button_new_with_label("hello");
+	view = gtk_text_view_new();
+	buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (view));
+
+	gtk_text_buffer_set_text(buffer, "This is some text", -1);
 	gtk_window_set_title (GTK_WINDOW (window), "Window");
 	gtk_window_set_default_size (GTK_WINDOW (window), 800, 800);
+	gtk_window_set_child(GTK_WINDOW(window), button);
+
 	gtk_widget_show(window);
 	gtk_widget_show(button);
 }
@@ -27,10 +31,10 @@ int main(int argc, char *argv[])
 	GtkApplication * app;
 	int status;
 
-	app = gtk_application_new ("org.gtk.ex", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect (app, "activate", G_CALLBACK (activate),  NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv);
-	g_object_unref (app);
+	app = gtk_application_new("my.app", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(app, "activate", G_CALLBACK (activate),  NULL);
+	status = g_application_run(G_APPLICATION (app), argc, argv);
+	g_object_unref(app);
 
 	return status;
 }
